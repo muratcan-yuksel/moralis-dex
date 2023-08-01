284bfc6d004074572f4ec6fdec83fdad1c7466e3
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-08-01 23:45:41 +0300

    .

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Logo from "../moralis-logo.svg";
+import Eth from "../eth.svg";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <div className="leftH">
+        <img src={Logo} alt="logo" className="logo" />
+        <div className="headerItem">Swap</div>
+        <div className="headerItem">Tokens</div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
